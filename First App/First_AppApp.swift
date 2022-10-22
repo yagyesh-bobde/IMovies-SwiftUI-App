f76efa591694e7640534154416a06a24241dfcc5
Pranav-Bobde <bobdep31@gmail.com>
2022-10-22 21:58:16 +0530

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Pranav Bobde on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
