8aa2278008bad526a1bb41d9ea34e4058b8a44e7
Pranav-Bobde <yagyeshSRbobde@gmail.com>
2022-11-10 13:25:34 +0530

    made ui changes

--- First App/ContentView.swift
+++ First App/ContentView.swift
@@ -14,25 +14,39 @@ struct ContentView: View {
     
     
     var body: some View {
-        Text("Hello, World")
         NavigationView{
             List {
                 ForEach(movies , id: \.id) {
                     movie in
-                    HStack{
-                        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/\(movie.poster_path ?? "")")
-                        )
-                        .scaledToFit()
-                        .frame(width: 10.0 , height: 10.0)
-                    }
-                    VStack{
-                        Text("\(movie.title ?? " ")")
-                        Text("\(movie.tagline ?? " ")")
-                    }
-                    
+                    MovieCard(movie: movie)
+//                    HStack{
+//                        AsyncImage(url: URL(
+//                           string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/\(movie.poster_path ?? "")"),
+//                           content:{ returnedImage in
+//                                returnedImage
+//                                .resizable()
+//                                .scaledToFit()
+//                                .frame(width:75, height: 50)
+//                        },
+//                           placeholder: {
+//                                    ProgressView()
+//                        })
+//                        VStack{
+//                            Text("\(movie.title ?? " ")")
+//                                .fontWidth(Font.Width.standard)
+//                                .multilineTextAlignment(.leading)
+//                            Text("\(movie.tagline ?? " ")")
+//                                .fontWeight(Font.Weight.light)
+//                                .multilineTextAlignment(.leading)
+//                                .foregroundColor(.gray)
+//                        }
+//                        .frame(alignment: .leading)
+//                        Spacer()
+//                        Image(systemName: "chevron.right")
+//                    }
                 }
-            }
-        }
+            }.lineSpacing(0)
+        }.navigationTitle("Popular Movies")
     }
 }
 
---
+++ First App/MovieCard.swift
@@ -0,0 +1,49 @@
+//
+//  MovieCard.swift
+//  First App
+//
+//  Created by Pranav Bobde on 10/11/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct MovieCard: View {
+    var movie: Movie
+    
+    var body: some View {
+        ZStack{
+            Rectangle().fill(Color.white).frame(height: 100)
+            HStack{
+                AsyncImage(url: URL(
+                   string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/\(movie.poster_path ?? "")"),
+                   content:{ returnedImage in
+                        returnedImage
+                        .resizable()
+                        .frame(width:50, height: 50)
+                        .scaledToFit()
+                },
+                   placeholder: {
+                            ProgressView()
+                })
+                HStack{
+                    VStack{
+                        Text("\(movie.title ?? " ")")
+                            .fontWidth(Font.Width.condensed)
+                            .multilineTextAlignment(.leading)
+                        Text("\(movie.tagline ?? " ")")
+                            .fontWeight(Font.Weight.light)
+                            .multilineTextAlignment(.leading)
+                            .foregroundColor(.gray)
+                    }.multilineTextAlignment(.leading)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                }
+//                .border()
+                .frame(alignment:.trailing)
+                .padding()
+                
+            }
+        }
+    }
+}
