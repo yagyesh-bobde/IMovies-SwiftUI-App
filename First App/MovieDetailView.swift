fbd6d09c1f36fea1f181855e0983be4acda7cae5
Yagyesh Bobde <90238742+yagyesh-bobde@users.noreply.github.com>
2022-11-11 20:35:20 +0530

    fix the syntax error+ added the scroll view the main view

--- First App/MovieDetailView.swift
+++ First App/MovieDetailView.swift
@@ -9,10 +9,30 @@ import SwiftUI
 
 struct MovieDetailView: View {
     var movie: Movie
+    
+    var columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
     var body: some View {
         
+        
+//        ScrollView{
+//            //            Color.gray
+//            PosterView(movie: movie)
+//            HStack{
+//                Text("Overview")
+//                    .multilineTextAlignment(.leading)
+//            }.background(Color.gray)
+//            Divider()
+//            Text("\(movie.overview ?? "")")
+//            MovieStaff(movie: movie)
+//            }
+//            .padding(5)
+//        }
         ZStack{
-//            Color.gray
+            //            Color.gray
             Grid(){
                 GridRow(alignment: .center){
                     PosterView(movie: movie)
@@ -21,76 +41,36 @@ struct MovieDetailView: View {
                     Divider()
                     Text("Overview")
                         .multilineTextAlignment(.leading)
-    //                    .background(Color.gray)
+                    //                    .background(Color.gray)
                     Divider()
                     Text("\(movie.overview ?? "")")
-                    VStack(alignment:.leading){
-//                      Directors
-                        Grid(){
-                            GridRow{
-                                ForEach(movie.directors ?? [] , id: \.id){ director in
-                                    PersonDetail(person: director, type: "Director")
-                                        .frame(width: 70 , height: 50)
-                                }
-                            }
-//                        Writers
-                            GridRow{
-                                ForEach(movie.writers ?? [] , id: \.id){ writer in
-                                    PersonDetail(person: writer, type: "Writer")
-                                        .frame(width: 100 , height: 50)
-                                }
-                        }
-//                        Cast
-                        GridRow{
-                            ForEach(movie.cast ?? [] , id: \.id){ cast in
-                                PersonDetail(person: cast, type: "Cast")
-                                    .frame(width: 100 , height: 50)
-                            }
-                    }
-                    }
+                    MovieStaff(movie: movie)
                 }
-                
-                
-//                GridRow{
-//
-//                    Grid(){
-//                        GridRow{
-//    //                        Directors
-//                        }
-//                        GridRow{
-//    //                        Writers
-//                        }
-//                        GridRow{
-//    //                        Cast
-//                        }
-//                    }
-//                }
+                .padding(14.0)
             }
-            .padding(14.0)
+            .padding(5)
         }
-        .padding(5)
     }
-}
-
-
-struct PosterView: View {
-    var movie: Movie
     
-    var body: some View {
+    
+    struct PosterView: View {
+        var movie: Movie
+        
+        var body: some View {
             ZStack(alignment: .center){
-//                Rectangle().fill(.white).frame(height: 250)
+                //                Rectangle().fill(.white).frame(height: 250)
                 VStack(alignment: .center){
                     AsyncImage(url: URL(
-                       string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/\(movie.poster_path ?? "")"),
-                       content:{ returnedImage in
-                            returnedImage
+                        string: "https://image.tmdb.org/t/p/w600_and_h900_bestv2/\(movie.poster_path ?? "")"),
+                               content:{ returnedImage in
+                        returnedImage
                             .resizable()
                             .frame(width:135, height: 225)
                             .scaledToFit()
                             .padding(.trailing)
                     },
-                       placeholder: {
-                                ProgressView()
+                               placeholder: {
+                        ProgressView()
                     })
                     HStack{
                         Text("\(movie.title ?? " ")")
@@ -101,24 +81,59 @@ struct PosterView: View {
                     Text("\(movie.certification ?? "") | \(movie.release_date ?? "") | \(movie.original_language ?? "")")
                         .foregroundColor(Color.gray)
                 }
-        }.navigationTitle("\(movie.title ?? " ")")
+            }.navigationTitle("\(movie.title ?? " ")")
+        }
     }
-}
-
-struct PersonDetail: View {
-    var person: people
-    var type: String
     
-    var body: some View {
-        VStack(alignment: .leading){
-            Text("\(person.name ?? "")")
-            Text("\(type)")
-                .foregroundColor(Color.gray)
+    struct PersonDetail: View {
+        var person: people
+        var type: String
+        
+        var body: some View {
+            VStack(alignment: .leading){
+                Text("\(person.name ?? "")")
+                Text("\(type)")
+                    .foregroundColor(Color.gray)
+            }
+        }
+    }
+    
+    struct MovieStaff: View {
+        var movie: Movie
+        
+        let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
+        
+        var body: some View {
+//            Movie Staff
+
+//                Directors
+                LazyVGrid(columns: columns, spacing: 5){
+                    ForEach(movie.directors ?? [] , id: \.id){ director in
+                        PersonDetail(person: director, type: "Director")
+                            .frame(width: 70 , height: 50)
+                    }
+                }
+//                Writers
+                LazyVGrid(columns: columns, spacing: 5){
+                    ForEach(movie.writers ?? [] , id: \.id){ writer in
+                        PersonDetail(person: writer, type: "Writer")
+                            .frame(width: 100 , height: 40)
+                    }
+                }
+//                Cast
+                LazyVGrid(columns: columns, spacing: 5){
+                    ForEach(movie.cast ?? [] , id: \.id){ cast in
+                        PersonDetail(person: cast, type: "Cast")
+                            .frame(width: 100 , height: 50)
+                    }
+                }
+            
         }
     }
 }
-
-
 
 
 struct MovieDetailView_Previews: PreviewProvider {
